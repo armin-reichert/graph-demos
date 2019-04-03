a0b6e0a6cb4b9ca00e5aaf91a2c65b89924d86c6
Armin Reichert <armin.reichert@web.de>
2019-04-03 18:59:56 +0200

    fix

--- graph-demos-pathfinder/src/main/java/de/amr/demos/graph/pathfinding/controller/action/ResetScene.java
+++ graph-demos-pathfinder/src/main/java/de/amr/demos/graph/pathfinding/controller/action/ResetScene.java
@@ -2,12 +2,15 @@ package de.amr.demos.graph.pathfinding.controller.action;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.Action;
+
 import de.amr.demos.graph.pathfinding.controller.PathFinderController;
 
 public class ResetScene extends PathFinderAction {
 
 	public ResetScene(PathFinderController controller) {
 		super(controller);
+		putValue(Action.NAME,	"Reset Scene");
 	}
 
 	@Override
